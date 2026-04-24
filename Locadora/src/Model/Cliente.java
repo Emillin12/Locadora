398f0216afb7fbc429d435ebf482feae496352dd
Emillin Barbosa <luizemillin@gmail.com>
2026-04-24 09:57:52 -0300

    feat: :sparkles: Fiz uma estrutura para a atividade
    Fiz uma pasta para o cliente , alguel e filmes . É a parte da atividade avaliativa, vamos avançar para o pr´ximo passo

--- Locadora/src/Model/Cliente.java
+++ Locadora/src/Model/Cliente.java
@@ -1,4 +1,28 @@
 package Model;
 
 public class Cliente {
+
+    private String Nome;
+    private String Telefone;
+
+    public Cliente(String nome, String telefone) {
+        Nome = nome;
+        Telefone = telefone;
+    }
+
+    public String getNome() {
+        return Nome;
+    }
+
+    public void setNome(String nome) {
+        Nome = nome;
+    }
+
+    public String getTelefone() {
+        return Telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        Telefone = telefone;
+    }
 }
--- Locadora/src/Model/Filme.java
+++ Locadora/src/Model/Filme.java
@@ -1,4 +1,11 @@
 package Model;
 
 public class Filme {
+
+    private String titulo;
+    private String genero;
+    private double ValorAluguel;
+
+    
+
 }
